acdcd21786b48029b914f3603e5cb8fe4958d4d2
Adriana Capriles <adrianacapriles@gmail.com>
2018-07-18 22:35:40 -0400

    working app

--- liri.js
+++ liri.js
@@ -1,4 +1,99 @@
 require("dotenv").config();
-
+var fs = require("fs");
+var keys = require("./keys.js")
+var request = require("request");
+var Spotify = require("node-spotify-api")
+var Twitter = require("twitter");
 var spotify = new Spotify(keys.spotify);
-  var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
+
+var action = process.argv[2];
+var toSearch = process.argv[3];
+
+//console.log('our action is: ' + action)
+
+switch (action) {
+    case "spotify-this-song":
+        callSpotify();
+        break;
+    case "my-tweets":
+        callTwitter();
+        break;
+    case "movie-this":
+        callOmdb();
+        break;
+};
+
+function callSpotify() {
+    var defaultSong = spotify.search({
+        type: 'track',
+        query: 'the sign',
+    });
+
+    if (toSearch === undefined) {
+        console.log(defaultSong);
+    }
+    else {
+        spotify.search({
+            type: 'track',
+            query: toSearch,
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            else {
+                console.log("\n *** 🔊  SPOTIFY  🎧  ***\n");
+                var foundTracks = data.tracks.items;
+                for (var i = 0; i < foundTracks.length; i++) {
+                    console.log('Title: ' + foundTracks[i].name);
+
+                    for (var j = 0; j < foundTracks[i].artists.length; j++) {
+                        console.log('Artist: ' + foundTracks[i].artists[j].name)
+                    }
+                    console.log('Album: ' + foundTracks[i].album.name);
+                    console.log('Preview Link: ' + foundTracks[i].preview_url)
+                    console.log("\n")
+                }
+            }
+            console.log("\n------------\n");
+        });
+    }
+}
+
+function callTwitter() {
+    var params = { screen_name: 'adrrriiii' };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (error) {
+            return console.log('Error occurred: ' + error);;
+        } else {
+            console.log("\n *** 🔎   TWITTER  📰  ***\n");
+            for (var i = 0; i < tweets.length; i++) {
+                console.log((i + 1) + ". " + tweets[i].text);
+            }
+            console.log("\n------------\n");
+        }
+    });
+}
+
+function callOmdb() {
+    console.log("\n *** 🎬  MOVIE  🎥  ***\n")
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + toSearch + "&y=&plot=short&apikey=trilogy";
+
+    //    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            console.log("Movie Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+}
